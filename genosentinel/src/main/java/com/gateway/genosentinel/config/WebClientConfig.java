c751b3c61bd68514eaf8b501d43acd91dc664915
Mateo Aguirre Ocampo <56123454+MateoAguirreO@users.noreply.github.com>
2025-12-01 22:58:54 -0500

    feat: Add Apache HttpClient5 dependency and configure RestTemplate for PATCH support

--- genosentinel/src/main/java/com/gateway/genosentinel/config/WebClientConfig.java
+++ genosentinel/src/main/java/com/gateway/genosentinel/config/WebClientConfig.java
@@ -1,7 +1,11 @@
 package com.gateway.genosentinel.config;
 
+import org.apache.hc.client5.http.classic.HttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -12,6 +16,9 @@ public class WebClientConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        // Usar Apache HttpClient5 para soportar método PATCH
+        HttpClient httpClient = HttpClientBuilder.create().build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+        return new RestTemplate(requestFactory);
     }
 }
